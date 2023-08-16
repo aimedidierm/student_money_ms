c6fd6567a555ad5173741478b4e7f89e841f8abf
Aime Didier Mugisha <73910734+aimedidierm@users.noreply.github.com>
2023-08-16 13:39:57 +0200

    Update students.blade.php

--- resources/views/school/students.blade.php
+++ resources/views/school/students.blade.php
@@ -30,8 +30,7 @@
                 <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
                     <div class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
                         <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
-                            Create new school account
-                        </h3>
+                            Create new student                        </h3>
                         <button type="button"
                             class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
                             data-modal-hide="addNewModal">
